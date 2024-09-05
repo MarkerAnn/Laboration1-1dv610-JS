4437b5ec18902f72e8d73d1b855762846c91ca66
Angelica Marker <am225cs@student.lnu.se>
2024-09-05 23:22:02 +0200

    changed  title name in index.html, add header. Moved all styles to tailwind.config, changed the animated background

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+// Take care of the users input, when user clicks on the button
+// check if the input is empty, not numbers and a valid string
+// Fetch to API
+// Show the result
+// Error handling
+// CDN safety for input
+// Add a loader
+// Add a reset button
+// Add a header
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gradient-1': '#3a4fd6',
+        'gradient-2': '#ce4db6',
+        'gradient-3': '#ae93f0',
+        'gradient-4': '#1289cd',
+        'gradient-5': '#1bbbd2',
+      },
+      animation: {
+        'gradient-bg': 'gradientBG 5s ease infinite',
+      },
+      keyframes: {
+        gradientBG: {
+          '0%, 100%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+        },
+      },
+      backgroundImage: {
+        'gradient-animation':
+          'linear-gradient(135deg, var(--tw-gradient-stops))',
+      },
+    },
   },
   plugins: [],
 }
